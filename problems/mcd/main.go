59c1c2e376474a9120c1abcae59ab9ac06585a6c
Christian <3003html@gmail.com>
2022-08-23 00:27:06 -0500

    algorith mcd finished

--- problems/mcd/main.go
+++ problems/mcd/main.go
@@ -15,3 +15,20 @@ func main() {
 	c1, _ := maxcd(a1, a2, 0)
 	fmt.Printf("MCD (%d - %d): %d", n, m, c1[len(c1)-1])
 }
+
+func maxcd(a1 []int, a2 []int, c int) ([]int, []int) {
+	if a1[c] == a2[c] {
+		return a1, a2
+	}
+	if a1[c] > a2[c] {
+		a1 = append(a1, a1[c]-a2[c])
+		a2 = append(a2, a2[c])
+		c++
+		return maxcd(a1, a2, c)
+	} else {
+		a1 = append(a1, a1[c])
+		a2 = append(a2, a2[c]-a1[c])
+		c++
+		return maxcd(a1, a2, c)
+	}
+}
