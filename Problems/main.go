49eecab14744b4b62a73a4fe366cd9186f7ad2db
Christian <3003html@gmail.com>
2022-08-22 22:44:05 -0500

    reordered files

---
+++ Problems/main.go
@@ -0,0 +1,60 @@
+package main
+
+/*Write a program that outputs all possibilities to put + or - or nothing between the numbers 1,2,…,9 (in this order) such that
+the result is 100. For example 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100.*/
+
+import (
+	"fmt"
+)
+
+var perm []string
+
+func main() {
+	inp := []string{"1", "2", "3", "+", "-", " "}
+	//num := 33 1 + 32
+	generate(len(inp), inp)
+	perm = deleter(perm)
+	fmt.Println(perm)
+}
+
+// Heap Algorithm
+func generate(n int, arr []string) {
+	if n == 1 {
+		s := ""
+		for _, e := range arr {
+			s += e
+		}
+		perm = append(perm, s)
+		fmt.Println(arr)
+	}
+	for i := 0; i < n; i++ {
+		generate(n-1, arr)
+		if n%2 == 0 {
+			aux := arr[i]
+			arr[i] = arr[n-1]
+			arr[n-1] = aux
+		} else {
+			aux2 := arr[0]
+			arr[0] = arr[n-1]
+			arr[n-1] = aux2
+		}
+	}
+}
+
+func deleter(arr []string) []string {
+	cont := false
+	for _, e := range arr {
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
+			cont = true
+		}
+	}
+	if !cont {
+		return arr
+	}
+	for i, e := range arr {
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
+			arr[i] = e[1:]
+		}
+	}
+	return deleter(arr)
+}
--- main.go
+++ main.go
@@ -4,29 +4,51 @@ import (
 	"fmt"
 )
 
-func main(){
-	arr := []string{"++-++++123+++","+321"}
-	fmt.Println(arr)
-	arr = delm(arr)
-	fmt.Println(arr)
+func main() {
+	arr1 := []string{"++-++++123++++++", "+321+++- +"}
+	fmt.Println(arr1)
+	arr1 = mI(arr1)
+	fmt.Println(arr1)
+}
+
+func mI(arr []string) []string {
+	flag := false
+	for _, e := range arr {
+		if string(e[len(e)-1]) == "+" || string(e[len(e)-1]) == "-" || string(e[len(e)-1]) == " " {
+			flag = true
+		}
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
+			flag = true
+		}
+	}
+	if !flag {
+		return arr
+	}
+	for i, e := range arr {
+		if string(e[len(e)-1]) == "+" || string(e[len(e)-1]) == "-" || string(e[len(e)-1]) == " " {
+			arr[i] = e[:(len(e) - 1)]
+		}
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
+			arr[i] = e[1:]
+		}
+	}
+	return mI(arr)
 }
 
 func delm(arr []string) []string {
 	cont := false
-	for _,e := range arr {
-                if string(e[0])=="+" || string(e[0])=="-" ||
-		string(e[0])==" " {
+	for _, e := range arr {
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
 			cont = true
-                }
-        }
+		}
+	}
 	if !cont {
 		return arr
 	}
-	for i,e := range arr {
-                if string(e[0])=="+" || string(e[0])=="-" || 
-		string(e[0])==" " {
-			arr[i]=e[1:]
-               	}
-       	}
-	return delm(arr)	
+	for i, e := range arr {
+		if string(e[0]) == "+" || string(e[0]) == "-" || string(e[0]) == " " {
+			arr[i] = e[1:]
+		}
+	}
+	return delm(arr)
 }
