dc2dcb84784ab63c5de95e4a593ae822d07cec2c
Christian <3003html@gmail.com>
2022-08-31 03:23:10 -0500

    statement added

---
+++ problems/fizzBuzz/main.go
@@ -0,0 +1,4 @@
+package main
+
+/* Escribe un programa que muestre los números del 1 al 100, sustituyendo los múltiplos de 3 por la última palabra "fizz",
+los múltiplos de 5 por "buzz" y los múltiplos de ambos por la palabra "fizzbuzz" */
