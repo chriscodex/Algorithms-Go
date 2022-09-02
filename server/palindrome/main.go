11877bfa8e2545e63d08a02185cacec4c5580d14
Christian <3003html@gmail.com>
2022-09-01 23:26:44 -0500

    palincrome algorithm done

--- server/palindrome/main.go
+++ server/palindrome/main.go
@@ -1,3 +1,11 @@
 package main
 
 /* Build a server where the client sends a word and the server responds if it is a palindrome or not */
+
+func palindrome(s string) bool {
+	str := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		str += string(s[i])
+	}
+	return str == s
+}
