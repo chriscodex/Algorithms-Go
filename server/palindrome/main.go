1d36b633d22b88d0b6e14137d248b3be54975d2f
Christian <3003html@gmail.com>
2022-09-01 23:27:40 -0500

    Palindrome handler created

--- server/palindrome/main.go
+++ server/palindrome/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 /* Build a server where the client sends a word and the server responds if it is a palindrome or not */
 
 func palindrome(s string) bool {
@@ -12,4 +18,23 @@ func palindrome(s string) bool {
 
 type palindromeModel struct {
 	Chain string `json:"chain"`
+}
+
+func palindromeHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	palMD := palindromeModel{}
+
+	decoder.Decode(&palMD)
+
+	res := palindrome(palMD.Chain)
+
+	response, err := json.Marshal(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
