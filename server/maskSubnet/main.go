50a4085092b7d459e1cea0d7a067c5863d761002
Christian <3003html@gmail.com>
2022-09-05 02:33:17 -0500

    algorithm maskSubNet created

--- server/maskSubnet/main.go
+++ server/maskSubnet/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"strconv"
 )
 
 func bitess(res int, p int) int {
@@ -21,4 +22,32 @@ func powTwo(n int) int {
 		}
 	}
 	return 0
+}
+
+func maskSubNet(num int) string {
+	p := powTwo(num)
+	if p <= 31 && p >= 24 {
+		s := ""
+		s += strconv.Itoa(bitess(31, p))
+		s += ".0.0.0"
+		return s
+	}
+	if p <= 23 && p >= 16 {
+		s := "255."
+		s += strconv.Itoa(bitess(23, p))
+		s += ".0.0"
+		return s
+	}
+	if p <= 15 && p >= 8 {
+		s := "255.255."
+		s += strconv.Itoa(bitess(15, p))
+		s += ".0"
+		return s
+	}
+	if p <= 7 && p >= 0 {
+		s := "255.255.255."
+		s += strconv.Itoa(bitess(7, p))
+		return s
+	}
+	return ""
 }
