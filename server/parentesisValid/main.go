65e808f1d036dac523dc66aa88412f38c54722f8
Christian <3003html@gmail.com>
2022-09-09 13:11:12 -0500

    set Header added

--- server/parentesisValid/main.go
+++ server/parentesisValid/main.go
@@ -56,6 +56,8 @@ func vpHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func main() {
