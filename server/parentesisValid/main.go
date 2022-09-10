4824bfdcd298220371923141143f272a4085ac1e
Christian <3003html@gmail.com>
2022-09-10 13:13:01 -0500

    write added

--- server/parentesisValid/main.go
+++ server/parentesisValid/main.go
@@ -59,6 +59,7 @@ func vpHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func main() {
