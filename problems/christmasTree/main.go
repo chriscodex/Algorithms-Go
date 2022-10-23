0577bc3bd58c8f703069ef2cf7dd11aeb9204036
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-22 21:37:45 -0500

    christamasTree problem done

---
+++ problems/christmasTree/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func addSpaces(str string, cant int) string {
+	var chain string
+	for i := 0; i < cant; i++ {
+		chain += " "
+	}
+	return chain + str
+}
+
+func asteriskGenerator(cant int) string {
+	var ast string
+	for i := 0; i < cant; i++ {
+		ast += "*"
+	}
+	return ast
+}
+
+func buildTree(numb int) {
+	var tree string
+	alt := 2 * numb
+	cant := numb
+	for j := 1; j <= alt; j += 2 {
+		tree = asteriskGenerator(j)
+		tree = addSpaces(tree, cant-1)
+		fmt.Println(tree)
+		cant--
+	}
+}
+
+func main() {
+	buildTree(6)
+}
